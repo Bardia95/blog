7beca3ae81db5d879ad8a9ee658733cdcd28125d
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2019-08-12 09:37:30 -0400

    fixing production storage

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,8 @@ Aws.config[:credentials] = Aws::Credentials.new(
   ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
 )
-Aws.config[:region]      = 'us-east-1'
+Aws.config[:s3_region]      = 'us-east-1'
+
+Aws::VERSION =  Gem.loaded_specs["aws-sdk-s3"].version
 
 S3 = Aws::S3::Client.new
